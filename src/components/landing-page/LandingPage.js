ceccef436d2f1cd80544c1eec83eb9c2187d308c
Gayathrey <72188275+Gayathrey@users.noreply.github.com>
2023-04-14 09:58:24 +0200

    vacationBooking

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -6,11 +6,12 @@ const LandingPage = () => {
       <div className="landing">
         <div className="wrapper">
           <div className="text-white d-flex flex-column text-center justify-content-center align-items-center h-100">
-            <h2>Welcome to Greenish Vacations</h2>
+            <h2 className="display-3">Welcome to Greenish Vacations</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-              cumque ex incidunt natus. Ullam optio, impedit facere accusamus
-              numquam fuga.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. A porro
+              et quibusdam ex quos voluptatibus quis sit quas distinctio at
+              dignissimos eaque assumenda ratione eos nostrum, possimus
+              consequuntur quisquam magnam!
             </p>
           </div>
         </div>
